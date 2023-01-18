51e8df02c2d377117c91e337e5fed8b14748e370
Shamanesss <mentulbenodorna@gmail.com>
2023-01-18 22:20:13 +0100

    arreglado el error del form

--- custom.js
+++ custom.js
@@ -1,28 +1,23 @@
 const num1 = document.querySelector('#numero1');
 const num2 = document.querySelector('#numero2');
-//const btnFooter =document.querySelector('#start');
 const rango = document.querySelector('#rango');
-//btnFooter.addEventListener('click', rangoNumeros);
 
 function rangoNumeros(){
-    console.log("hola");
-    
-    let valor1 = 34//parseInt(num1.value);
-    let valor2 = 55//parseInt(num2.value);
+        
+    let valor1 = parseInt(num1.value);
+    let valor2 =parseInt(num2.value);
     let resultado=' ';
     let i;
+    resultado += "<ul>";
 
      
       if (valor1<=valor2){
          for( i=valor1;i<=valor2;i++){
-             resultado += i+" ";
-            
+             resultado += `<li> ${i} </li>`;   
          }
       }else if (valor1>=valor2){
           for( i=valor1;i>=valor2;i--){
-             resultado += i +" "; 
-              
-             
+             resultado += `<li> ${i} </li>`;    
           }
 
      }rango.innerHTML =resultado;
